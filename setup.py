f71f0842ca7fdf8b1b34b335adc4b90a17e26e4d
Michael J. Harms <harmsm@gmail.com>
2017-08-23 13:01:09 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+__description__ = \
+"""
+"""
+__version__ = "0.0.2"
 import sys 
+
 if sys.version_info[0] < 3:
     sys.exit('Sorry, Python < 3.x is not supported')
 
@@ -8,13 +13,13 @@ from Cython.Distutils import build_ext
 
 setup(name="peplearn",
       packages=find_packages(),
-      version='0.0.2',
+      version=__version__,
       description="machine learning for peptide binding using physiochemical properties",
-      long_description=open("README.rst").read(),
+      long_description=__description__,
       author='Michael J. Harms',
       author_email='harmsm@gmail.com',
       url='https://github.com/harmslab/peplearn',
-      download_url='https://github.com/harmslab/peplearn/tarball/0.0.2',
+      download_url="https://github.com/harmslab/peplearn/archive/{}.tar.gz".format(__version__),
       install_requires=["numpy","localcider"],
       package_data={"peplearn":["*.json","features/data/*.json"]},
       zip_safe=False,
