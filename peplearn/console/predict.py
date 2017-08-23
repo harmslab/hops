190deea38ed8364ff014102d993eb05748165f69
Michael J. Harms <harmsm@gmail.com>
2017-08-23 14:01:01 -0700

    adding new console control scripts

---
+++ peplearn/console/predict.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+__description__ = \
+"""
+Predict binding from a pre-calculated feature file and trained peplearn model.
+"""
+__author__ = "Michael J. Harms"
+__date__ = "2017-08-23"
+
+import peplearn as pl
+
+import os, sys, pickle, arparse
+
+def main(argv=None):
+
+    if argv is None:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(description=__description__)
+    
+    # Positionals
+    parser.add_argument("feature_pickle",help="pickled feature file used to train model")
+    parser.add_argument("trained_pickle",help="pickled trained model")
+
+    # Options 
+    parser.add_argument("-o","--outdir",help="output directory",action="store",type=str,default=".")
+    parser.add_argument("-n","--numcpu",help="number of cpus to use.  if -1, use all cpus",action="store",type=int,default=-1)
+
+    args = parser.parse_args(argv)
+
+    # Figure out number of threads
+    if args.numcpu == -1:
+        num_threads = os.cpu_count()
+    else:
+        num_threads = args.numcpu
+
+    # Create output directory (if necessary)
+    try:
+        os.mkdir(args.outdir)
+    except FileExistsError:
+        pass
+
+    # Figure out output files
+    feature_base = os.path.split(feature_pickle)[1]
+    trained_model_base = os.path.split(trained_pickle)[1]
+
+    predictions_file = os.path.join(out_dir,"{}_{}_predictions.txt".format(feature_base,trained_model_base))
+
+    if os.path.isfile(predictions_file):
+        err = "out file '{}' already exists.\n".format(predictions_file)
+        raise FileExistsError(err)
+
+    forest = pickle.load(open(trained_pickle,"rb"))
+
+    # Calculate features
+    features = pickle.load(open(feature_pickle,"rb"))
+    features.add_classes([-2,0])
+    
+    # Make predictions
+    predictions = forest.predict(features)
+    seqs = list(predictions.keys())
+    seqs.sort()
+    
+    f = open(predictions_file,'w')
+    for s in seqs:
+        f.write("{} {}\n".format(s,predictions[s]))
+    f.close()
+
+if __name__ == "__main__":
+    main() 
---
+++ peplearn/console/train.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+__description__ = \
+"""
+Use peplearn and sklearn to train a random forest model against a
+pre-calculated feature file.  If the user specifies specifies category
+breaks, use a classifier.  Otherwise, use a continuous regression 
+model.
+"""
+__author__ = "Michael J. Harms"
+__date__ = "2017-08-23"
+
+import peplearn as pl
+
+from sklearn import ensemble
+
+#In the future, can be modified to use basically any sklearn model  
+#from sklearn import neural_network, svm, naive_bayes
+
+import os, sys, pickle, argparse
+
+def main(argv=None):
+
+    parser = argparse.ArgumentParser(description=__description__)
+    
+    # Positionals
+    parser.add_argument("feature_pickle",help="pickled feature file used to train model")
+
+    # Options 
+    parser.add_argument("-o","--outdir",help="output directory",action="store",type=str,default=".")
+    parser.add_argument("-n","--numcpu",help="number of cpus to use.  if -1, use all cpus",action="store",type=int,default=-1)
+    parser.add_argument("-w","--weight",help="weight regression according to weights in the feature file",action="store_true")
+    parser.add_argument("-e","--estimators",help="number of estimators to use",action="store",type=int,default=40)
+    parser.add_argument("-b","--break", 
+                        help="add category break. specify multiple --break to train for multiple categories. if no breaks are specified, model will be continuous",
+                        type=float,
+                        action="append",
+                        dest="breaks",
+                        default=[])
+
+    args = parser.parse_args(argv)
+
+    if len(args.breaks) == 0:
+        calc_type = "regressor"
+    else:
+        calc_type = "classifier"
+
+    # Figure out number of threads
+    if args.numcpu == -1:
+        num_threads = os.cpu_count()
+    else:
+        num_threads = args.numcpu
+
+    # Figure out what kinds of weights to use
+    weight_type = "even"
+    if args.weight:
+        weight_type = "file"
+
+    # Create output directory (if necessary)
+    try:
+        os.mkdir(args.outdir)
+    except FileExistsError:
+        pass
+
+    # Figure out output files
+    training_file = os.path.split(args.feature_pickle)[1]
+    model_out_file = os.path.join(args.outdir,"{}_model.pickle".format(training_file))
+    log_out_file = os.path.join(args.outdir,"{}.log".format(training_file))
+
+    if os.path.isfile(model_out_file):
+        err = "out file '{}' already exists.\n".format(model_out_file)
+        raise FileExistsError(err)
+
+    if os.path.isfile(log_out_file):
+        err = "out file '{}' already exists.\n".format(log_out_file)
+        raise FileExistsError(err)
+
+    features = pickle.load(open(args.feature_pickle,'rb'))
+
+    # Choose model type.  In the future, could include a bunch of possible models
+    if calc_type == "classifier":
+        features.add_classes(args.breaks)
+        model = ensemble.RandomForestClassifier(n_estimators=args.estimators,
+                                                n_jobs=num_threads) 
+        #model = neural_network.MLPClassifier()
+        #model = svm.SVC() #gamma=2, C=1)
+        #model = naive_bayes.GaussianNB()
+        #model = ensemble.AdaBoostClassifier()
+        #model = svm.SVC(gamma=2, C=1)
+    else:
+        model = ensemble.RandomForestRegressor(n_estimators=args.estimators,
+                                               n_jobs=num_threads) 
+        #model = ensemble.GradientBoostingRegressor(n_estimators=100) 
+        #model = svm.SVR(kernel='rbf',C=1e-3,gamma=0.1) 
+        
+    # Train models
+    forest = pl.MachineLearner(model)
+    forest.train(features,weights=weight_type)
+
+    # Write out model pickle
+    f = open(model_out_file,"wb")
+    pickle.dump(forest,f)
+    f.close()
+
+    # Write out human-readable log file 
+    f = open(log_out_file,"w")
+    f.write(forest.log)
+    f.close()
+ 
+if __name__ == "__main__":
+    main() 
