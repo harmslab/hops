2a157d2d27c2036a37a525406e186724f490b9e1
Michael J. Harms <harmsm@gmail.com>
2020-06-03 06:50:21 -0700

    fixed a few bugs

--- hops/console/kmerize.py
+++ hops/console/kmerize.py
@@ -13,7 +13,7 @@ def create_kmers(seq,kmer_size):
     """
     Create a set of kmers from a sequence.
     """
-    
+
     return [seq[i:(i+kmer_size)] for i in range(len(seq)-kmer_size+1)]
 
 def parse_proteome(fasta_file,kmer_size=12,out_base="kmers",seq_per_file=50000,num_to_write=1000000):
@@ -21,64 +21,64 @@ def parse_proteome(fasta_file,kmer_size=12,out_base="kmers",seq_per_file=50000,n
     Read a uniprot fasta file of protein sequences and break into a set of
     kmers.
     """
-  
+
     all_kmers = {}
-    seq_name = None 
+    seq_name = None
     current_sequence = []
 
     # Parse fasta file, splitting into kmers as we go
     with open(fasta_file) as infile:
         for l in infile:
-            
+
             if l.startswith(">"):
                 if seq_name is not None:
 
                     sequence = "".join(current_sequence)
                     kmer_list = create_kmers(sequence,kmer_size)
-                    
+
                     for k in kmer_list:
                         try:
                             all_kmers[k].append(seq_name)
                         except KeyError:
-                            all_kmers[k] = [seq_name]    
-            
+                            all_kmers[k] = [seq_name]
+
                 current_sequence = []
                 seq_name = l[1:].strip()
             else:
                 if seq_name is None or l.strip() == "":
                     continue
                 current_sequence.append(l.strip())
-    
+
     if seq_name is not None:
 
         sequence = "".join(current_sequence)
         kmer_list = create_kmers(sequence,kmer_size)
-        
+
         for k in kmer_list:
             try:
                 all_kmers[k].append(seq_name)
             except KeyError:
-                all_kmers[k] = [seq_name]    
-    
+                all_kmers[k] = [seq_name]
+
     # Sort kmers
     to_sort = [(len(all_kmers[k]),k) for k in all_kmers.keys()]
     to_sort.sort(reverse=True)
 
-    # kmers occuring more than once
-    kmers = [k[1] for k in to_sort if k[0] > 1]
+    # kmers 
+    kmers = [k[1] for k in to_sort]
 
     if len(kmers) > num_to_write:
         kmers = kmers[:num_to_write]
     else:
-    
-        # If there are more single kmers than the total we want to get, grab a 
-        # random selection of them. 
+
+        # If there are more single kmers than the total we want to get, grab a
+        # random selection of them.
         single_kmers = [k[1] for k in to_sort if k[0] == 1]
         if num_to_write - len(kmers) > 0:
             to_grab = num_to_write - len(kmers)
             random.shuffle(single_kmers)
             kmers.extend(single_kmers[:to_grab])
-  
+
     out = []
     counter = 0
     for k in kmers:
@@ -96,7 +96,7 @@ def parse_proteome(fasta_file,kmer_size=12,out_base="kmers",seq_per_file=50000,n
             out_file = "{}_{}.kmers".format(out_base,counter)
             print(counter,len(kmers))
             sys.stdout.flush()
-        
+
             f = open(out_file,'w')
             f.write("".join(out))
             f.close()
@@ -104,14 +104,14 @@ def parse_proteome(fasta_file,kmer_size=12,out_base="kmers",seq_per_file=50000,n
             out = []
 
         counter += 1
-            
+
 
     out_file = "{}_{}.kmers".format(out_base,counter)
 
     f = open(out_file,'w')
     f.write("".join(out))
     f.close()
-            
+
 
 def main(argv=None):
     """
@@ -122,11 +122,11 @@ def main(argv=None):
         argv = sys.argv[1:]
 
     parser = argparse.ArgumentParser(description=__description__)
-    
+
     # Positionals
     parser.add_argument("fasta_file",help="fasta file to be turned into kmers")
 
-    # Options 
+    # Options
     parser.add_argument("-o","--outbase",help="base name for output files",action="store",type=str,default=None)
     parser.add_argument("-k","--kmersize",help="kmer size",action="store",type=int,default=12)
     parser.add_argument("-s","--seqperfile",help="number of sequences per output file",action="store",
--- hops/observations.py
+++ hops/observations.py
@@ -1,6 +1,6 @@
 __description__ = \
 """
-Class storing observations and allowing calculation of features. 
+Class storing observations and allowing calculation of features.
 """
 __author__ = "Michael J. Harms"
 __usage__ = "2017-08-23"
@@ -18,27 +18,27 @@ class Observations:
     """
     Class for creating and holding machine learning training/test sets.
     """
-    
+
     def __init__(self,observation_file,kfold_size=10,value_type=None):
         """
         Initialize the class.
 
-        observation_file: text file with observations. 
-        kfold_size: split the data into k_fold sets for cross validation.  
+        observation_file: text file with observations.
+        kfold_size: split the data into k_fold sets for cross validation.
                     1/kfold_size observations are set aside as a true test set.
                     The remaining observations are used for (kfold_size-1)
                      cross validation.
         value_type: type of value in the dataset.  If None, this is determined
                     from the file itself.
         """
-       
+
         self._observation_file = observation_file
         self._kfold_size = kfold_size
-        self._value_type = value_type 
- 
+        self._value_type = value_type
+
         # load in all of the observations
         self._load_observations()
-        
+
         # initial features.  everyone gets a 0
         self._features = np.zeros((len(self._raw_values),1),dtype=float)
         self._feature_names = np.array(["dummy"])
@@ -46,24 +46,24 @@ class Observations:
 
         # set initial breaks to None
         self._breaks = None
-        
+
     def _new_test_set(self):
         """
         Create a test set by randomizing indexes and selecting a subset to be used as test versus
         training.
         """
-        
+
         self._k_fraction = 1.0/self._kfold_size
         self._k_length = int(np.floor(len(self._indexes)*self._k_fraction))
-               
+
         np.random.shuffle(self._indexes)
-        
-        
+
+
     def add_features(self,features_instance):
         """
         Calculate features using a features_instance and append to the total feature set.
         """
-      
+
         self._features_engines.append(features_instance)
 
 
@@ -88,15 +88,15 @@ class Observations:
     def calc_features(self,num_threads=1,block_size=3000):
         """
         Calculate the features for every observation using the appended Features
-        instances. 
+        instances.
 
         num_threads: number of threads to run on
         block_size: number of seqeuences to put on a single process
         """
-    
-        # Create a list of feature functions... 
+
+        # Create a list of feature functions...
         feature_names = []
-        self._feature_functions = [] 
+        self._feature_functions = []
         num_features = 0
         for e in self._features_engines:
             num_features += e.num_features
@@ -105,12 +105,12 @@ class Observations:
 
         # Create a compiled list of feature names
         self._feature_names = np.concatenate(feature_names)
-       
-        # If enough threads are specified that only a few threads would start, 
-        # make the block size smaller 
+
+        # If enough threads are specified that only a few threads would start,
+        # make the block size smaller
         if len(self._sequences)//num_threads < block_size:
             block_size = len(self._sequences)//num_threads + 20
-        
+
         # Split squences in to blocks of block_size
         block_edges = []
         for i in range(0,len(self._sequences),block_size):
@@ -124,7 +124,7 @@ class Observations:
 
         # Go through each sequence
         for i in range(len(block_edges)-1):
-            
+
             first_seq = block_edges[i]
             last_seq =  block_edges[i+1]
 
@@ -132,9 +132,9 @@ class Observations:
             proc_list.append(Process(target=self._calc_features_on_thread,
                                      args=(first_seq,last_seq,queue_list[-1])))
             proc_list[-1].start()
-           
+
             # If we've capped our number of threads, wait until one of the
-            # processes finishes to move on 
+            # processes finishes to move on
             if (len(queue_list) == num_threads) or (i == len(block_edges) - 2):
 
                 waiting = True
@@ -142,10 +142,10 @@ class Observations:
 
                     # Go through queues
                     for j, q in enumerate(queue_list):
-                      
-                        # Try to get output on queue.  If output is there, get 
+
+                        # Try to get output on queue.  If output is there, get
                         # the output and then remove the associated process and
-                        # queue 
+                        # queue
                         try:
                             out.append(q.get(block=True,timeout=0.1))
                             p = proc_list.pop(j)
@@ -161,19 +161,19 @@ class Observations:
                     # completely empty before proceeding
                     if len(queue_list) != 0 and i == (len(block_edges) - 2):
                         waiting = True
- 
+
         # Load results into self._features
         self._features = np.zeros((len(self._sequences),num_features),dtype=float)
         for o in out:
             self._features[o[0]:o[1],:] = o[2]
 
- 
+
     def _load_observations(self):
         """
         Load in a file of observations.  Expected to have format:
-        
+
         SEQ [VALUE] [VALUE_WEIGHT]
-        
+
         Blank lines and lines starting with # are ignored.
         """
 
@@ -182,10 +182,10 @@ class Observations:
         weight_list = []
         with open(self._observation_file) as f:
             for l in f:
-            
+
                 if l.strip() == "" or l.startswith("#"):
                     continue
-            
+
                 col = l.split()
 
                 sequence = col[0].strip()
@@ -195,7 +195,7 @@ class Observations:
                     try:
                         raw_value = float(col[1].strip())
                         if raw_value.is_integer():
-                            self._value_type = int  
+                            self._value_type = int
                         else:
                             self._value_type = float
                     except IndexError:
@@ -204,6 +204,8 @@ class Observations:
                     except ValueError:
                         self._value_type = str
                         raw_value = self._value_type(col[1].strip())
+                else:
+                    raw_value = self._value_type(col[1].strip())
 
                 try:
                     weight = float(col[2])
@@ -215,94 +217,94 @@ class Observations:
                 weight_list.append(weight)
 
         self._sequences = np.array(seq_list)
-       
+
         self._raw_values = np.array(raw_value_list,dtype=self._value_type)
         self._weights = np.array(weight_list,dtype=float)
         self._indexes = np.arange(len(self._raw_values))
-       
+
         self._values = np.copy(self._raw_values)
-     
+
         self._new_test_set()
-        
+
     def add_cutoff_filter(self,logic=">=",cutoff=None):
         """
-        Add a cutoff, removing a subset of the data.  This is nondestructive.  Filters can 
-        be added one on top of the other sequentially.  
-        
+        Add a cutoff, removing a subset of the data.  This is nondestructive.  Filters can
+        be added one on top of the other sequentially.
+
         logic is a string version of a logical operator (>=, <=, >, <, ==, !=).
-            
-        cutoff is the value against which the data are compared. 
+
+        cutoff is the value against which the data are compared.
         """
-        
+
         filter_ops = {">=":operator.ge,
                       "<=":operator.le,
                       ">" :operator.gt,
                       "<" :operator.lt,
                       "==":operator.eq,
                       "!=":operator.ne}
-        
+
         if cutoff is not None:
             f = filter_ops[logic](self.values,cutoff)
             self._indexes = self._indexes[f]
-                                              
+
             self._new_test_set()
 
     def remove_filters(self):
         """
         Remove any filters that have been applied.
-        """          
-            
+        """
+
         self._indexes = np.arange(len(self._raw_values))
         self._new_test_set()
-    
+
     def add_classes(self,breaks):
         """
         Convert into classes defined by breaks.  self.values (and test_values, training_values)
         will now return discrete classes rather than float values.  These can be removed by
-        remove_classes.  
+        remove_classes.
         """
 
         self._breaks = copy.copy(breaks)
 
         classes = np.zeros(len(self._raw_values))
-        
-        # Values below the first break        
+
+        # Values below the first break
         class_number = 0
         first_class = self._raw_values < self._breaks[0]
         if np.sum(first_class) > 0:
             classes[first_class] = class_number
             class_number += 1
-            
+
         # Values between each break
         for i in range(len(self._breaks)-1):
             c1 = self._raw_values >= self._breaks[i]
             c2 = self._raw_values < self._breaks[i+1]
-            
+
             classes[c1*c2] = class_number
             class_number += 1
-        
+
         # Values above the top break
         last_class = self._raw_values >= self._breaks[-1]
         classes[last_class] = class_number
-        
+
         self._values = classes
-        
+
     def remove_classes(self):
         """
         Remove discrete class calls so values returns floats.
         """
         self._values = np.copy(self._raw_values)
         self._breaks = None
-   
+
     @property
     def breaks(self):
         """
         Return breaks between classes.
         """
-       
+
         if not self._breaks is None:
             return self._breaks
-    
+
         return None
 
     @property
@@ -310,47 +312,47 @@ class Observations:
         """
         Return names of all features.
         """
-        
+
         return self._feature_names
- 
+
     @property
     def sequences(self):
         """
         Return all sequences, with appropriate cutoffs applied.
         """
-    
+
         return self._sequences[self._indexes]
-    
+
     @property
     def features(self):
         """
         Return all sequence features, with appropriate cutoffs applied.
         """
-            
+
         return self._features[self._indexes,:]
-            
+
     @property
     def values(self):
         """
         Return all values, with appropriate cutoffs or class filters applied.
         """
-        
+
         return self._values[self._indexes]
-    
+
     @property
     def weights(self):
         """
         Return all value weights, with appropriate cutoffs or class filters applied.
         """
-        
+
         return self._weights[self._indexes]
-            
+
     @property
     def test_sequences(self):
         """
         Return sequences for test set.
         """
-        
+
         return self.sequences[0:self._k_length]
 
     @property
@@ -358,7 +360,7 @@ class Observations:
         """
         Return features for test set.
         """
-        
+
         return self.features[0:self._k_length,:]
 
     @property
@@ -366,15 +368,15 @@ class Observations:
         """
         Return values for test set.
         """
-        
-        return self.values[0:self._k_length]    
+
+        return self.values[0:self._k_length]
 
     @property
     def test_weights(self):
         """
         Return weights for test set.
         """
-        
+
         return self.weights[0:self._k_length]
 
     @property
@@ -382,17 +384,17 @@ class Observations:
         """
         Return sequences for training set.
         """
-        
+
         return self.sequences[self._k_length:]
-          
+
     @property
     def training_features(self):
         """
         Return features for training set.
         """
-        
+
         return self.features[self._k_length:,:]
-        
+
     @property
     def training_values(self):
         """
@@ -404,9 +406,9 @@ class Observations:
     @property
     def training_weights(self):
         """
-        Return weights for training set. 
+        Return weights for training set.
         """
-        
+
         return self.weights[self._k_length:]
 
     @property
@@ -414,14 +416,14 @@ class Observations:
         """
         Number of categories for k-fold training and validation.
         """
-    
+
         return self._kfold_size
 
     def _meta_k_training(self,array_to_slice,k):
         """
-        Meta function that slices an array that returns a k-fold 
+        Meta function that slices an array that returns a k-fold
         training set.  This should only be called by the public methods
-        in this class (things like get_k_training_sequences). 
+        in this class (things like get_k_training_sequences).
         """
 
         # Only use the real training set
@@ -431,16 +433,16 @@ class Observations:
             err = "k must be between 0 and {}.\n".format(0,self._kfold_size-2)
             raise ValueError(err)
 
-        start =  real_training[:(k*self._k_length)]        
+        start =  real_training[:(k*self._k_length)]
         finish = real_training[((k+1)*self._k_length):]
-        
+
         return np.concatenate((start,finish))
-     
+
     def _meta_k_test(self,array_to_slice,k):
         """
-        Meta function that slices an array that returns a k-fold 
+        Meta function that slices an array that returns a k-fold
         test set.  This should only be called by the public methods
-        in this class (things like get_k_test_sequences). 
+        in this class (things like get_k_test_sequences).
         """
 
         # Only use the real training set
@@ -450,13 +452,13 @@ class Observations:
             err = "k must be between 0 and {}.\n".format(0,self._kfold_size-2)
             raise ValueError(err)
 
-        return real_training[(k*self._k_length):((k+1)*self._k_length)]        
+        return real_training[(k*self._k_length):((k+1)*self._k_length)]
 
     def get_k_training_sequences(self,k):
         """
         Get the kth set of training sequences for k-fold cross training and
         validation.
-        
+
         k must be between 0 and total k -1.
         """
 
@@ -466,7 +468,7 @@ class Observations:
         """
         Get the kth set of training features for k-fold cross training and
         validation.
-        
+
         k must be between 0 and total k -1.
         """
 
@@ -476,7 +478,7 @@ class Observations:
         """
         Get the kth set of training values for k-fold cross training and
         validation.
-        
+
         k must be between 0 and total k -1.
         """
 
@@ -486,7 +488,7 @@ class Observations:
         """
         Get the kth set of training weights for k-fold cross training and
         validation.
-        
+
         k must be between 0 and total k -1.
         """
 
@@ -496,7 +498,7 @@ class Observations:
         """
         Get the kth set of test sequences for k-fold cross test and
         validation.
-        
+
         k must be between 0 and total k -1.
         """
 
@@ -506,7 +508,7 @@ class Observations:
         """
         Get the kth set of test features for k-fold cross test and
         validation.
-        
+
         k must be between 0 and total k -1.
         """
 
@@ -516,7 +518,7 @@ class Observations:
         """
         Get the kth set of test values for k-fold cross test and
         validation.
-        
+
         k must be between 0 and total k -1.
         """
 
@@ -526,7 +528,7 @@ class Observations:
         """
         Get the kth set of test weights for k-fold cross test and
         validation.
-        
+
         k must be between 0 and total k -1.
         """
 
@@ -570,4 +572,3 @@ def calc_features(sequence_data,use_flip_pattern=True,use_sliding_windows=12,
     obs.calc_features(num_threads)
 
     return obs
-
